7e7504d9b635d074ffd473ca9e4505d36715cb35
Hiroaki Ono <hiono@student.42adel.org.au>
2024-06-27 12:03:27 +0930

    modified msg

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hiono <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:13:21 by hiono             #+#    #+#             */
-/*   Updated: 2024/06/25 18:34:08 by hiono            ###   ########.fr       */
+/*   Updated: 2024/06/27 12:02:40 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 
 int main()
 {
-	std::cout << "--- Animal Construct ---" << std::endl;
+	std::cout << "[  Animal Construct ]" << std::endl;
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	std::cout << std::endl;
 
-	std::cout << "--- Animal Functions ---" << std::endl;
+	std::cout << "[ Animal Functions ]" << std::endl;
 	std::cout << "j's type: " << j->getType() << " " << std::endl;
 	std::cout << "i's type: " << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
@@ -32,18 +32,18 @@ int main()
 	meta->makeSound();
 	std::cout << std::endl;
 
-	std::cout << "--- WrongAnimal Construct ---" << std::endl;
+	std::cout << "[ WrongAnimal Construct ]" << std::endl;
 	const WrongAnimal* wrongmeta = new WrongAnimal();
 	const WrongAnimal* wrongi = new WrongCat();
 	std::cout << std::endl;
 
-	std::cout << "--- WrongAnimal Functions ---" << std::endl;
+	std::cout << "[ WrongAnimal Functions ]" << std::endl;
 	std::cout << "worngi's type: " << wrongi->getType() << " " << std::endl;
 	wrongi->makeSound(); //will output the cat sound!
 	wrongmeta->makeSound();
 	std::cout << std::endl;
 
-	std::cout << "--- Destruct ---" << std::endl;
+	std::cout << "[ Destruct ]" << std::endl;
 	delete meta;
 	delete j;
 	delete i;
